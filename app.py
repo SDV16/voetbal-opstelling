b7d6f43fc347929ec77525dadb03899680e19ec4
SDV16 <117817496+SDV16@users.noreply.github.com>
2026-04-12 00:24:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -374,7 +374,7 @@ if st.button("Genereer opstellingen"):
                 # =============================
                 # KOLOMMEN (HIER GEBEURT HET)
                 # =============================
-                col_opstelling, col_wissels = st.columns([3,2])
+                col_opstelling, col_wissels = st.columns([1,2])
             
                 # =============================
                 # LINKS: OPSTELLING
